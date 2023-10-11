6053892a6d4eee61ffc508fd26356ba7f9092ae7
Tobias Karlsson <github@hoidi.se>
2023-10-11 19:44:14 +0200

    Add Forecast type

---
+++ src/types/forecast.ts
@@ -0,0 +1,38 @@
+import type { Coordinates } from './coordinates';
+
+export type Forecast = {
+	supplierName: string;
+	coordinates: Coordinates;
+	time: Date;
+	forecast: Weather[];
+};
+
+export type Weather = {
+	time: Date;
+	percipitation: Percipitation;
+	temperature: number;
+	windSpeed: number;
+	windDirection: number;
+};
+
+type Percipitation = {
+	min: number;
+	max: number;
+	mean: number;
+	median: number;
+};
+
+export const emptyWeather = (): Weather => {
+	return {
+		time: new Date(),
+		percipitation: {
+			min: -1,
+			max: -1,
+			mean: -1,
+			median: -1
+		},
+		temperature: -1,
+		windSpeed: -1,
+		windDirection: -1
+	};
+};
